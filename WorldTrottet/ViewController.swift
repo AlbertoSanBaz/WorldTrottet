a369ef7fabd44604854bebc7336346d2681c4489
Berts S Baz <albertosanchezbazan@macbook-pro-de-alberto.local>
2018-03-26 23:01:35 +0200

    Initial Commit

---
+++ WorldTrottet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTrottet
+//
+//  Created by Alberto Sánchez Bazán on 26/3/18.
+//  Copyright © 2018 Alberto Sánchez Bazán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

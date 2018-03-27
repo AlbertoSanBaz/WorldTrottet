9d36b3694aa50d693d3c8b7bc01ba11d848bcfbf
Berts S Baz <albertosanchezbazan@macbook-pro-de-alberto.local>
2018-03-27 17:15:43 +0200

    Number Formatting

--- WorldTrottet/ConversionViewController.swift
+++ WorldTrottet/ConversionViewController.swift
@@ -11,11 +11,46 @@ import UIKit
 class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textFlied: UITextField!
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelsiusLabel()
+    }
     
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
         
-        if let text = textField.text, !text.isEmpty {
-            celsiusLabel.text = text
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
         } else {
             celsiusLabel.text = "???"
         }

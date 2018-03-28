d557355ffb1184d40f3a2c3592bab930b8dd90ef
Berts S Baz <albertosanchezbazan@macbook-pro-de-alberto.local>
2018-03-28 02:13:30 +0200

    Finish chapter 5

--- WorldTrottet/ConversionViewController.swift
+++ WorldTrottet/ConversionViewController.swift
@@ -36,6 +36,7 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("ConversionViewController loaded its view.")
         updateCelsiusLabel()
     }
     
---
+++ WorldTrottet/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrottet
+//
+//  Created by Alberto Sánchez Bazán on 28/3/18.
+//  Copyright © 2018 Alberto Sánchez Bazán. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
